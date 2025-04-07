4b292ce81a3387f6018b62a2dc543226a0e0f82f
Aadithyan <rak34316@gmail.com>
2025-04-07 23:29:22 +0530

    updated

---
+++ Day1-2Ass/script.js
@@ -0,0 +1,7 @@
+//eIq0sFXyVweL4C2zYOruJzWdEujVzFVG8GiVMjGdRgsaYKjXhXK2oiGg
+
+import { createClient } from 'pexels';
+
+const client = createClient('YOUR_API_KEY');
+
+//client.photos.show({ id: 2014422 }).then(photo => {...})
